ecb636bcda8101da80df7b56ec2a8801da3ff30e
Roy kimosop <kimutairoy87@gmail.com>
2026-05-13 11:52:33 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
-import {Routes, Route} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Display
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Discovery from './components/Discovery';
+import Library from './components/Library';
+import About from './components/About';
+
+export default function App() {
+    return (
+        <div className="min-h-screen flex flex-col md:">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Discovery />} />
+                <Route path="/library" element={<Library />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    )
+}
+
